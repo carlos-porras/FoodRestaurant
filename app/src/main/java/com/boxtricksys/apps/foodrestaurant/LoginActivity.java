1628569e8d55221903f6a3e17cf288b6475598c9
Carlos Porras <carlos.porras.0508@gmail.com>
2016-07-16 11:35:47 -0500

    Attach Onclick Event

--- app/src/main/java/com/boxtricksys/apps/foodrestaurant/LoginActivity.java
+++ app/src/main/java/com/boxtricksys/apps/foodrestaurant/LoginActivity.java
@@ -2,12 +2,38 @@ package com.boxtricksys.apps.foodrestaurant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    EditText user;
+    EditText password;
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        this.setContentView(R.layout.activity_login);
+        this.initComponents();
+
+    }
+
+    private void initComponents(){
+        this.user = (EditText) findViewById(R.id.userEditText);
+        this.password = (EditText) findViewById(R.id.PasswordEditText);
+        this.login = (Button) findViewById(R.id.loginButton);
+        this.login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.loginButton:
+                Toast.makeText(getApplicationContext(), "Bien", Toast.LENGTH_LONG);
+                break;
+        }
     }
 }
