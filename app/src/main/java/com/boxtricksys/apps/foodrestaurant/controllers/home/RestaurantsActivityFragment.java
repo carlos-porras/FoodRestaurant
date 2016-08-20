990a98886e7d8d5df190242d62cb44b1a0f08784
Carlos Porras <carlos.porras.0508@gmail.com>
2016-08-20 15:09:19 -0500

    Validation get Json and convert to java object

--- app/src/main/java/com/boxtricksys/apps/foodrestaurant/controllers/home/RestaurantsActivityFragment.java
+++ app/src/main/java/com/boxtricksys/apps/foodrestaurant/controllers/home/RestaurantsActivityFragment.java
@@ -6,18 +6,29 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.boxtricksys.apps.foodrestaurant.R;
 import com.boxtricksys.apps.foodrestaurant.api.Requests;
 import com.boxtricksys.apps.foodrestaurant.api.RestConstants;
+import com.boxtricksys.apps.foodrestaurant.models.Restaurant;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class RestaurantsActivityFragment extends Fragment {
 
+    private List<Restaurant> restaurants;
     public RestaurantsActivityFragment() {
     }
 
@@ -33,10 +44,24 @@ public class RestaurantsActivityFragment extends Fragment {
         requests.setOnRequestSuccess(new Requests.Listener() {
             @Override
             public void OnRequestSuccess(JSONObject jsonObject) {
-                Log.i("RESPUESTA", jsonObject.toString());
+                validateJsonResponse(jsonObject);
             }
         });
 
         requests.execute(RestConstants.RESTAURANTS_ENDPOINT, RestConstants.POST_REQUEST);
+    }
+
+    private void validateJsonResponse(JSONObject jsonObject) {
+        try {
+            if(jsonObject.getString("status").equalsIgnoreCase("success")){
+                JSONArray JsonArrayRestaurants = jsonObject.getJSONArray("data");
+                Type typeTokenListRestaurants = new TypeToken<List<Restaurant>> () {}.getType();
+                restaurants = new Gson().fromJson(JsonArrayRestaurants.toString(), typeTokenListRestaurants);
+            }else{
+                Toast.makeText(getActivity().getApplicationContext(), R.string.toast_string_error_get_info_restaurant, Toast.LENGTH_LONG).show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
